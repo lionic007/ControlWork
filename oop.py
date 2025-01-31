4161ea1bddf1b6b8e0640f17ec80eb576a3d326a
NGC7891 <ngc7891@gmail.com>
2025-01-31 14:57:06 +0600

    feat[task-4]:  abstract & refactor docs README.md

--- oop.py
+++ oop.py
@@ -71,3 +71,33 @@ bike = Bicycle()
 
 print(move(car))  # Вывод: Car is driving
 print(move(bike))  # Вывод: Bicycle is pedaling
+
+
+# Задание 4. Абстракция
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        return self.width * self.height
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return pi * (self.radius ** 2)
+
+# Пример использования:
+rect = Rectangle(10, 5)
+circle = Circle(7)
+
+print(rect.area())      # Вывод: 50
+print(circle.area())    # Вывод: ~153.94
