3eb795e131a8ea7543d1491775ea4d360bdace40
NGC7891 <ngc7891@gmail.com>
2025-01-31 14:52:55 +0600

    feat[task-2 & task-3]:  inheritance polymorphism

--- oop.py
+++ oop.py
@@ -1,3 +1,7 @@
+from math import pi
+from abc import ABC, abstractmethod
+
+
 # Инкапсуляция
 class Person:
     def __init__(self):
@@ -20,3 +24,50 @@ try:
     p.set_age(-5)  # Должна быть ошибка или предупреждение
 except ValueError as e:
     print(e)
+
+# Наследие
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        
+    def speak(self):
+        return "I am an animal"
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+    
+# Пример использования:
+dog = Dog("Buddy")
+cat = Cat("Kitty")
+
+print(dog.name, dog.speak())  # Вывод: Buddy Woof
+print(cat.name, cat.speak())  # Вывод: Kitty Meow
+
+# Задание 3. Полиморфизм
+class Vehicle(ABC):
+    @abstractmethod
+    def move(self):
+        pass
+
+class Car(Vehicle):
+    def move(self):
+        return "Car is driving"
+
+class Bicycle(Vehicle):
+    def move(self):
+        return "Bicycle is pedaling"
+
+def move(vehicle):
+    return vehicle.move()
+
+# Пример использования:
+car = Car()
+bike = Bicycle()
+
+print(move(car))  # Вывод: Car is driving
+print(move(bike))  # Вывод: Bicycle is pedaling
